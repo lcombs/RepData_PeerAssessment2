c1072274bd120df89249a954ce1aecd0cec4eb72
lcombs <lisa.combs@gmail.com>
2016-01-26 22:26:40 -0500

    readme R delete

--- ReadMe.R
+++
@@ -1,5 +0,0 @@
-ReadMe
-
-This project involves exploring the U.S. National Oceanic and Atmospheric Administration's (NOAA) storm database. This database tracks characteristics of major storms and weather events in the United States, including when and where they occur, as well as estimates of any fatalities, injuries, and property damage.
-
-The projects are published at: http://rpubs.com/lcombs/weathereffects
